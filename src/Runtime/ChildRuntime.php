206e0d0da12a44ad2f5efbb996c9fad19dde428f
Fred Myerscough <oniice@gmail.com>
2019-09-26 18:14:26 +0100

    fix: ensure that output length is always an int

--- src/Runtime/ChildRuntime.php
+++ src/Runtime/ChildRuntime.php
@@ -5,7 +5,7 @@ use Spatie\Async\Runtime\ParentRuntime;
 try {
     $autoloader = $argv[1] ?? null;
     $serializedClosure = $argv[2] ?? null;
-    $outputLength = $argv[3] ?? (1024 * 10);
+    $outputLength = $argv[3] ? intval($argv[3]) : (1024 * 10);
 
     if (! $autoloader) {
         throw new InvalidArgumentException('No autoloader provided in child process.');
