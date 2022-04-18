5f96f3458760de5dd9559acfe0424da484de4f65
gyorgybakocs <gyorgy.bakocs@transprnt.com>
2022-04-18 15:38:49 +0200

    corrcted

--- src/Output/SerializableException.php
+++ src/Output/SerializableException.php
@@ -15,18 +15,37 @@ class SerializableException
     /** @var string */
     protected $trace;
 
+    /** @var string */
+    protected $details;
+
+    /** @var int */
+    private $code;
+
+    /** @var null|Throwable */
+    private $previous;
+
     public function __construct(Throwable $exception)
     {
         $this->class = get_class($exception);
         $this->message = $exception->getMessage();
+        $this->code = $exception->getCode();
+        $this->previous = $exception->getPrevious();
         $this->trace = $exception->getTraceAsString();
+        if (method_exists($exception,'getDetails')) {
+            $this->details = $exception->getDetails();
+        }
     }
 
     public function asThrowable(): Throwable
     {
         try {
             /** @var Throwable $throwable */
-            $throwable = new $this->class($this->message."\n\n".$this->trace);
+            if (!empty($this->details)) {
+                $throwable = new $this->class($this->message, $this->code, $this->previous, $this->details);
+            } else {
+                $throwable = new $this->class($this->message, $this->code, $this->previous);
+            }
+
         } catch (Throwable $exception) {
             $throwable = new ParallelException($this->message, $this->class, $this->trace);
         }
