ac545b6b23a2a8ebb933a086bbe25e9ff4967f50
Marc Hagen <hello@marchagen.nl>
2024-08-29 00:50:13 +0200

    Catch `php://output` and add to output. Any writes to `php://stdout` will break the unserialization of child process results

--- src/Runtime/ChildRuntime.php
+++ src/Runtime/ChildRuntime.php
@@ -2,6 +2,16 @@
 
 use Spatie\Async\Runtime\ParentRuntime;
 
+// php://stdout does not obey output buffering. Any output would break
+// unserialization of child process results in the parent process.
+if (!defined('STDOUT')) {
+    define('STDOUT', fopen('php://temp', 'w+b'));
+    define('STDERR', fopen('php://stderr', 'wb'));
+}
+
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 'stderr');
+
 try {
     $autoloader = $argv[1] ?? null;
     $serializedClosure = $argv[2] ?? null;
@@ -23,7 +33,9 @@ try {
 
     $task = ParentRuntime::decodeTask($serializedClosure);
 
+    ob_start();
     $output = call_user_func($task);
+    ob_end_clean();
 
     $serializedOutput = base64_encode(serialize(['output' => $output]));
 
